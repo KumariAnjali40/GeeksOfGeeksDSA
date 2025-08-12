0507821a9f335089e2b99da7dc1b4a57c2a79a26
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-12 15:17:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.java
+++ Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.java
@@ -1,16 +1,33 @@
 class Solution {
     public static boolean matSearch(int mat[][], int x) {
         // your code here
-        boolean isPresent = false;
+        // boolean isPresent = false;
         
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[0].length; j++){
-                if(mat[i][j] == x){
-                    isPresent = true;
-                    break;
-                }
+        // for(int i = 0; i < mat.length; i++){
+        //     for(int j = 0; j < mat[0].length; j++){
+        //         if(mat[i][j] == x){
+        //             isPresent = true;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return isPresent;
+        
+       int rows = mat.length;
+        int cols = mat[0].length;
+
+        int i = 0, j = cols - 1;
+
+        while (i < rows && j >= 0) {
+            if (mat[i][j] == x) {
+                return true;
+            } else if (mat[i][j] > x) {
+                j--; 
+            } else {
+                i++; 
             }
         }
-        return isPresent;
+
+        return false;
     }
 }
