300f84b0dd94dfe5ee99a4d1f3520ed6efab2eaf
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-15 00:12:13 +0530

    Update reverse-a-linked-list.java

--- Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.java
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.java
@@ -11,23 +11,36 @@ class Node {
 class Solution {
     Node reverseList(Node head) {
         
-        Stack <Integer> stack = new Stack<>();
+        // Stack <Integer> stack = new Stack<>();
         
-        Node temp = head;
+        // Node temp = head;
         
-        while(temp != null){
-            stack.push(temp.data);
-            temp = temp.next;
+        // while(temp != null){
+        //     stack.push(temp.data);
+        //     temp = temp.next;
+        // }
+        
+        // temp = head;
+        
+        // while(temp != null){
+        //     temp.data = stack.pop();
+        //     temp = temp.next;
+        
+        // }
+        
+        // return head;
+
+        Node curr = head;
+        Node front;
+        Node prev = null;
+        
+        while(curr != null){
+            front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
         }
         
-        temp = head;
-        
-        while(temp != null){
-            temp.data = stack.pop();
-            temp = temp.next;
-        
-        }
-        
-        return head;
+        return prev;
     }
 }
