706734dfc3dc358f64a47762b0d3dac6a554a0fd
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-16 20:07:41 +0530

    Update delete-middle-of-linked-list.java

--- Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.java
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.java
@@ -11,27 +11,51 @@ class Solution {
         // This is method only submission.
         // You only need to complete the method.
         
-        if (head.next == null)
+        // if (head.next == null)
+        //     return null;
+        
+        // Node curr = head;
+        // Node temp = head;
+        
+        // int length = 0;
+        // while(curr != null) {
+        //     length++;
+        //     curr = curr.next;
+        // }
+        
+        // int midIndex = length/2;
+        
+        
+        
+        // for(int i = 0; i < midIndex-1; i++){
+        //     temp = temp.next;
+        // }
+        
+        // temp.next = temp.next.next;
+        
+        // return head;
+
+
+         if(head == null){
             return null;
-        
-        Node curr = head;
-        Node temp = head;
-        
-        int length = 0;
-        while(curr != null) {
-            length++;
-            curr = curr.next;
         }
         
-        int midIndex = length/2;
-        
-        
-        
-        for(int i = 0; i < midIndex-1; i++){
-            temp = temp.next;
+        if(head.next == null){
+            return null;
         }
         
-        temp.next = temp.next.next;
+        Node fast = head;
+        Node slow = head;
+        Node prev = null;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        
+        
+        prev.next = slow.next;
         
         return head;
         
