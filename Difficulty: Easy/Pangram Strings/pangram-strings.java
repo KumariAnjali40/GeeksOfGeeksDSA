2bbc2e31f24fc9f7fd7fcb54e0ec124b7548ffde
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-18 18:14:48 +0530

    Update pangram-strings.java

--- Difficulty: Easy/Pangram Strings/pangram-strings.java
+++ Difficulty: Easy/Pangram Strings/pangram-strings.java
@@ -3,25 +3,35 @@
 class Solution {
     int isPanagram(String S) {
         // code here
-         boolean[] seen = new boolean[26];
+         // boolean[] seen = new boolean[26];
          
-         String s = S.toLowerCase();
+         // String s = S.toLowerCase();
          
-         for(int i = 0; i < s.length(); i++){
+         // for(int i = 0; i < s.length(); i++){
              
-             char ch = s.charAt(i);
+         //     char ch = s.charAt(i);
              
-             if(ch >= 'a' && ch <= 'z'){
-                 seen[ch-'a'] = true;
-             }
-         }
+         //     if(ch >= 'a' && ch <= 'z'){
+         //         seen[ch-'a'] = true;
+         //     }
+         // }
          
-         for(boolean i : seen){
-             if(!i){
-                 return 0;
-             }
-         }
+         // for(boolean i : seen){
+         //     if(!i){
+         //         return 0;
+         //     }
+         // }
          
-         return 1;
+         // return 1;
+
+        Set<Character> set = new HashSet<>();
+        
+        for(char ch : S.toLowerCase().toCharArray()){
+            if(ch >= 'a' && ch <= 'z'){
+                set.add(ch);
+            }
+        }
+        
+        return set.size() == 26 ? 1: 0;
     }
 };
