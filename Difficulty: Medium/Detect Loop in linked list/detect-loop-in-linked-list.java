3e6f86ab71a586f041b743d4f06359ab425386b7
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-18 11:37:35 +0530

    Update detect-loop-in-linked-list.java

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
@@ -24,6 +24,20 @@ class Solution {
         
         // return false;
         
+        // Node slow = head;
+        // Node fast = head;
+        
+        // while(slow != null && fast !=null && fast.next !=null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+            
+        //     if(slow == fast){
+        //         return true;
+        //     }
+        // }
+        
+        // return false;
+
         Node slow = head;
         Node fast = head;
         
