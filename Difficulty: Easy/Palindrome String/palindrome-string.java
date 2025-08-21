4e4ec7321ee749608e10f7c04b198a6dbe7cae8a
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-22 00:21:05 +0530

    Update palindrome-string.java

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,22 +1,37 @@
 class Solution {
     boolean isPalindrome(String s) {
         // code here
-       boolean isPal =  recPalidrome(0,s.length()-1, s);
+       // boolean isPal =  recPalidrome(0,s.length()-1, s);
+
+        boolean isPal =  recPalidrome(0, s);
        
        return isPal;
     }
     
-    public static boolean recPalidrome (int left, int right, String s){
+    // public static boolean recPalidrome (int left, int right, String s){
         
-        if(left >= right){
+    //     if(left >= right){
+    //         return true;
+    //     }
+        
+        
+    //     if(s.charAt(left) != s.charAt(right)){
+    //         return false;
+    //     }
+        
+    //    return recPalidrome(left+1, right-1, s);
+    // }
+
+     public static boolean recPalidrome (int i , String s){
+        
+        if(i >= s.length()/2){
             return true;
         }
         
-        
-        if(s.charAt(left) != s.charAt(right)){
+        if(s.charAt(i) != s.charAt(s.length()-i-1)){
             return false;
         }
         
-       return recPalidrome(left+1, right-1, s);
+        return recPalidrome(i+1, s);
     }
 }
