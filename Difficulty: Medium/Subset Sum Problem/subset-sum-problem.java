ecdcdd2c29178f1dc96af6e652c4292eb8527f80
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-09-11 11:26:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -3,42 +3,67 @@ class Solution {
     static Boolean isSubsetSum(int arr[], int sum) {
         // code here
         
-        Boolean[][] dp = new Boolean[arr.length][sum + 1];
+        // Boolean[][] dp = new Boolean[arr.length][sum + 1];
+        
+        boolean[] dp = new boolean[sum + 1];
+        
+        dp[0] = true;
         
         
-       boolean ans =  solve(arr, sum, 0, dp);
+    //   boolean ans =  solve(arr, sum, 0, dp);
        
-       return ans;
+    //   return ans;
+    
+    
+    //bottom up 
+    
+      for(int num : arr){
+          
+          for(int i = sum; i >= num; i-- ){
+              
+              if(dp[i - num]){
+                  dp[i] = true;
+              }
+          }
+      }
+      
+      return dp[sum];
         
     }
     
-    public static boolean solve(int [] arr, int sum , int index,  Boolean[][] dp){
+    // public static boolean solve(int [] arr, int sum , int index,  Boolean[][] dp){
        
-       if(sum == 0){
-           return true;
-       }
+    //   if(sum == 0){
+    //       return true;
+    //   }
        
-       if(index == arr.length){
-           return false;
-       }
+    //   if(index == arr.length){
+    //       return false;
+    //   }
        
-       if(dp[index][sum] != null){
-           return dp[index][sum];
-       }
+    //   if(dp[index][sum] != null){
+    //       return dp[index][sum];
+    //   }
        
-       boolean include = false;
+    //   boolean include = false;
        
-       if(sum >= arr[index]){
-           include = solve(arr, sum - arr[index], index+1, dp);
-       }
+    //   if(sum >= arr[index]){
+    //       include = solve(arr, sum - arr[index], index+1, dp);
+    //   }
        
-       boolean exclude = solve(arr, sum, index+1, dp);
+    //   boolean exclude = solve(arr, sum, index+1, dp);
        
        
-        dp[index][sum] = include || exclude;
+    //     dp[index][sum] = include || exclude;
        
-       return dp[index][sum];
+    //   return dp[index][sum];
+    
+
+    
+     
+    
+    
         
         
-    }
+    // }
 }
