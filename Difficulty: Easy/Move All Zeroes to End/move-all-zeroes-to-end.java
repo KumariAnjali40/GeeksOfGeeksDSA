11a76255de063cb0472750ead7dcacd5aea81554
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-09-17 19:18:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -27,5 +27,20 @@ class Solution {
         for(int i = 0; i < store.length; i++){
             arr[i] = store[i];
         }
+        
+        // int j = 0;
+        
+        // for(int i = 0; i < n; i++){
+        //     if(arr[i] != 0){
+        //         arr[j] = arr[i];
+        //         j++;
+        //     }
+            
+        // }
+        
+        // while(j < n){
+        //     arr[j] = 0;
+        //     j++;
+        // }
     }
 }
