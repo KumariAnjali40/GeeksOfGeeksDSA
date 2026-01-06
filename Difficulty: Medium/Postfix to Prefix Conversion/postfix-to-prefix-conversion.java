f69fcdce0821886cfc37b7a373d9baee8795f1b7
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2026-01-06 19:04:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.java
@@ -0,0 +1,43 @@
+// User function Template for Java
+
+class Solution {
+    static String postToPre(String post_exp) {
+        // code here
+        String ans = convert(post_exp);
+        
+        return ans;
+    }
+    
+    public static String convert(String s){
+        Stack<String> st = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            // +, -, *, /
+            if(ch != '+' && ch != '-' && ch != '*' && ch != '/'){
+                String str = Character.toString(ch);
+                st.push(str);
+            }else {
+                String newString = "";
+                if(!st.isEmpty()){
+                    String ch1 = st.pop();
+                    String ch2 = st.pop();
+                    
+                   newString += ch;
+                   newString += ch2+ch1;
+                   
+                   st.push(newString);
+                }
+            }
+        }
+        
+        String finalAns = "";
+        
+        while (!st.isEmpty()){
+            finalAns += st.pop();
+        }
+        
+        return finalAns;
+    }
+}
