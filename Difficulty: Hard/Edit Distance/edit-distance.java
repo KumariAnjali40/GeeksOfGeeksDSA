cd03d6d95887ab3b082052b09fe5e69a6178a0c9
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2026-02-13 12:19:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int editDistance(String s1, String s2) {
+        // Code here
+        
+        int n = s1.length();
+        int m = s2.length();
+        
+        int [][] dp = new int [n+1][m+1];
+        
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = i;
+        }
+        
+        for(int i = 0; i <= m; i++){
+            dp[0][i] = i;
+        }
+        
+        return solve(s1, s2, n, m, dp);
+    }
+    
+    public static int solve(String s1, String s2, int n, int m, int [][] dp){
+        
+        // if(i == 0) return j;
+        // if(j == 0) return i;
+        
+        // if(s1.charAt(i-1) == s2.charAt(j-1)){
+        //     return solve(s1, s2, i-1, j-1);
+        // }
+        
+        // return 1 + Math.min(
+        //     solve(s1, s2, i, j - 1),      // Insert
+        //     Math.min(
+        //         solve(s1, s2, i - 1, j),  // Delete
+        //         solve(s1, s2, i - 1, j - 1) // Replace
+        //     )
+        // );
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+                }
+                
+                
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
